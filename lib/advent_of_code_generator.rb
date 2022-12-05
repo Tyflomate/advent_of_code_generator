429c0db495eafccce300f61381de4cfa31df92b0
Tyflomate <florian@tymate.com>
2022-12-05 19:38:23 +0100

    feat: add solve command

--- lib/advent_of_code_generator.rb
+++ lib/advent_of_code_generator.rb
@@ -2,7 +2,7 @@
 
 require "thor"
 require_relative "advent_of_code_generator/utils"
-require_relative "advent_of_code_generator/generate_day"
+require_relative "advent_of_code_generator/commands"
 
 module AdventOfCodeGenerator
   class Error < StandardError; end
@@ -12,6 +12,7 @@ module AdventOfCodeGenerator
     include AdventOfCodeGenerator::Parser
 
     map "g" => :generate
+    map "s" => :solve
 
     option :day, :type => :numeric, :required => true, :aliases => "-d"
     option :year, :type => :numeric, :aliases => "-y"
@@ -25,7 +26,23 @@ module AdventOfCodeGenerator
       say e.message, :red
     end
 
-    register(AdventOfCodeGenerator::GenerateDay, "generateDay", "generateDay", "Generate days files")
+    option :day, :type => :numeric, :required => true, :aliases => "-d"
+    option :year, :type => :numeric, :aliases => "-y"
+    option :part, :type => :numeric
+    desc "solve", "Print results of your solution"
+    def solve
+      day = parse_day(options[:day])
+      year = parse_year(options[:year])
+
+      invoke 'solveDay', ['start'], :day => day, :year => year, :part => options[:part]
+    rescue AdventOfCodeGenerator::Error => e
+      say e.message, :red
+    end
+
+    register(AdventOfCodeGenerator::Commands::GenerateDay, "generateDay", "generateDay", "Generate days files")
+
+    desc "solveDay", 'Prints results for your solution'
+    subcommand "solveDay", AdventOfCodeGenerator::Commands::SolveDay
 
     def self.exit_on_failure?
       true
---
+++ lib/advent_of_code_generator/commands.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require_relative 'commands/base_command'
+require_relative 'commands/generate_day'
+require_relative 'commands/solve_day'
